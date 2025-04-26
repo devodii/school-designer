1eccf985874741677af1d3b25e1431921ea5943c
devodii <emmanuelodii80@gmail.com>
2025-04-26 19:32:20 +0100

    feat: added mixin configuration for compound components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
-import { AuthForm } from "@/components/auth-form"
+import { TextField } from "@/components/text-field"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-20 p-6 md:p-12 lg:p-24">
       <section className="flex w-full flex-col gap-8 lg:flex-row">
-        <div className="flex max-w-2xl flex-col gap-5">
+        <div className="flex max-w-2xl flex-col gap-3">
           <h4 className="text-4xl font-semibold">🎒 Design your school life</h4>
           <p className="text-secondary-foreground text-[17px]">
             AI-made notebooks, planners, timetables, and class themes that actually match your vibe. Download & print,
             or flex it with your friends
           </p>
+          <ul className="">
+            <li>🎨 Design custom notebook + diary covers from your photo or prompt.</li>
+            <li>🧠 Generate smart study timetables based on your schedule.</li>
+            <li>📕 Print as PDFs, save to your phone, or just share on socials</li>
+          </ul>
         </div>
 
-        <div className="max-w-sm flex-1">
-          <AuthForm />
+        <div className="flex max-w-sm flex-1 flex-col gap-3">
+          <TextField labelHtmlFor="email" inputId="email" labelText="Email" />
+          <Button className="w-full">Continue</Button>
         </div>
-      </section>
-
-      <section className="flex flex-col gap-2">
-        <h3 className="w-full text-center text-3xl font-semibold">Your study tools are finally aesthetic.</h3>
-        <ul className="">
-          <li>🎨 Design custom notebook + diary covers from your photo or prompt.</li>
-          <li>🧠 Generate smart study timetables based on your schedule.</li>
-          <li>📕 Print as PDFs, save to your phone, or just share on socials</li>
-        </ul>
       </section>
     </div>
   )
---
+++ src/components/text-field.tsx
@@ -0,0 +1,29 @@
+import React, { ComponentProps } from "react"
+
+import { Input, type InputProps } from "@/components/ui/input"
+import { type LabelProps, Label } from "@/components/ui/label"
+import { MixinProps, splitProps } from "@/lib/mixin"
+import { cn } from "@/lib/utils"
+
+interface TextFieldProps
+  extends ComponentProps<"div">,
+    MixinProps<"input", InputProps>,
+    MixinProps<"label", Omit<LabelProps, "children">>,
+    MixinProps<"error", Omit<ComponentProps<"span">, "children">> {
+  labelText: string
+  errorText?: string
+}
+
+export const TextField = ({ labelText, errorText, ...mixProps }: TextFieldProps) => {
+  const { input, label, error, ...rest } = splitProps(mixProps, "input", "label", "error")
+
+  return (
+    <div className={cn("flex w-full flex-col gap-2", rest.rest.className)} {...rest}>
+      <Label className={cn("w-full flex-1", label.className)} {...label}>
+        {labelText}
+      </Label>
+      <Input {...input} />
+      {errorText && <span {...error}>{errorText}</span>}
+    </div>
+  )
+}
