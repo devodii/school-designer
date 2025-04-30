1f7298a7cf5c6604976a5065c6bb7a6a01bfc434
devodii <emmanuelodii80@gmail.com>
2025-04-30 12:23:32 +0100

    feat: added tab list component

---
+++ src/components/menu-tabs.tsx
@@ -0,0 +1,46 @@
+import { ComponentProps, ComponentType, createElement } from "react"
+
+import { MixinProps, splitProps } from "@/lib/mixin"
+import { cn } from "@/lib/tw-merge"
+import { TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs"
+import { Root as Tabs } from "@radix-ui/react-tabs"
+
+interface MenuTabsProps
+  extends ComponentProps<typeof Tabs>,
+    MixinProps<"list", ComponentProps<typeof TabsList>>,
+    MixinProps<"trigger", ComponentProps<typeof TabsTrigger>>,
+    MixinProps<"content", ComponentProps<typeof TabsContent>> {
+  tabs: { label: string; value: string; component: ComponentType }[]
+}
+
+const MenuTabs = ({ tabs, ...mixinProps }: MenuTabsProps) => {
+  const { list, trigger, content, rest } = splitProps(mixinProps, "list", "trigger", "content")
+
+  return (
+    <Tabs {...rest} className={cn("flex w-full flex-col items-center justify-center gap-2", rest.className)}>
+      <TabsList
+        {...list}
+        className={cn("w-full rounded-none border-b-[2px] border-b-[#35383F] bg-inherit px-0", list.className)}
+      >
+        {tabs.map(({ value, label }) => (
+          <TabsTrigger {...trigger} key={value} className={cn("w-full", trigger.className)} value={value}>
+            {label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+
+      {tabs.map(({ value, component }) => (
+        <TabsContent
+          {...content}
+          key={value}
+          className={cn("flex w-full items-center justify-center", content.className)}
+          value={value}
+        >
+          {createElement(component)}
+        </TabsContent>
+      ))}
+    </Tabs>
+  )
+}
+
+export default MenuTabs
