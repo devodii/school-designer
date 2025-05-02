058fe695c1adf451be4cadca1601bea3f4de6afc
devodii <emmanuelodii80@gmail.com>
2025-05-02 19:13:15 +0100

    chore: remove redirects from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,7 @@
-import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-
-  if (pathname === "/waitlist") return NextResponse.next()
-
-  return NextResponse.redirect(new URL("/waitlist", request.url))
+  // todo: do stuff
 }
 
 export const config = {
